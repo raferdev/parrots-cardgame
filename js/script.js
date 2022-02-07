b2698673c5d66f5dc1adf533d400f1f8a0dcc2e1
Rafael <rafinha_fern@hotmail.com>
2022-02-07 15:13:58 -0300

    vs 3.0
    Aplicação da dinamica do jogo e cronometro

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
 let quantidadeCartas = 0;
 let arrayCartas = []
-let cartas = [0,1,2,3,4,5,6];
+let cartas = [0, 1, 2, 3, 4, 5, 6];
+let contador = 0;
+let carta1 = 99;
+let carta2 = 98;
+let paresAchados = 0;
+let idCarta1 = 99;
+let idCarta2 = 99;
+const relogio = document.querySelector(".relogio");
+let intervalo = null;
+let terminarContagem = 0;
+let tempoDeJogo = 0;
+
+function aumentarContagem() {
+  if(terminarContagem === 1) {
+    clearInterval(intervalo);
+  } else {
+      tempoDeJogo ++
+    relogio.innerHTML = parseInt(relogio.innerHTML) + 1; 
+  }
+}
+intervalo = setInterval(aumentarContagem, 1000);
+
+
+
+
+
+
 
 function funcaoQuantidadeCartas() {
     quantidadeCartas = prompt('Com quantas cartas você quer jogar? Deve ser de 4 a 14 cartas e apenas pares.');
@@ -19,32 +45,96 @@ funcaoQuantidadeCartas()
 function geradorCartas(numero) {
     if (numero >= 4 && numero <= 14 && numero % 2 === 0) {
         embaralhar(cartas);
-        for ( i = 0 ; i < parseInt(numero/2); i++) {
+        for (i = 0; i < parseInt(numero / 2); i++) {
             arrayCartas.push(cartas[i])
             arrayCartas.push(cartas[i])
         }
         embaralhar(arrayCartas)
-        
+
         let cartaHtml = document.querySelector('section');
-        for (i = 0; i < arrayCartas.length ; i++) {
+
+        for (i = 0; i < arrayCartas.length; i++) {
             cartaHtml.innerHTML += `
-		<div class = "cardPrimario" onclick= "virarcarta(this)" >
+		<div data-identifier="card" class = "cardPrimario carta${i}" onclick= "virarcarta(this, ${arrayCartas[i]}, ${i})" >
         <div class= "cardSecundario">
-            <div class="card cardFrente"></div>
-            <div class="card cardVerso verso${arrayCartas[i]}"></div>
+            <div data-identifier="back-face" class="card cardFrente"></div>
+            <div data-identifier="front-face" class="card cardVerso verso${arrayCartas[i]}"></div>
         </div>
         </div>
 		`
         }
     } else {
-        alert('Número Inválido, tente novamente :P')
         funcaoQuantidadeCartas()
     }
 }
 
-function virarcarta(carta) {
-    carta.classList.add('rotacao')
+function virarcarta(carta, verso, idCarta) {
+    function cartaVirada(carta, verso, idCarta) {
+        if (contador == 2) {
+            return
+        } else if (contador == 0) {
+            carta1 = verso;
+            idCarta1 = idCarta
+            carta.classList.add('rotacao')
+            contador++
+            return
+        } else if (contador == 1) {
+            carta2 = verso;
+            idCarta2 = idCarta
+            carta.classList.add('rotacao')
+            contador++
+            if (carta1 == carta2) {
+                setTimeout(boa, 1000)
+                return
+            } else {
+                setTimeout(desvirarCarta, 2000);
+            }
+            return
+        }
+
+    }
+    cartaVirada(carta, verso, idCarta)
 }
-function embaralhar(array){
-    array.sort(()=> Math.random() - 0.5);
+
+function desvirarCarta() {
+    let desvirar1 = document.querySelector(".carta"+idCarta1);
+    let desvirar2 = document.querySelector(".carta"+idCarta2);
+    desvirar1.classList.remove('rotacao');
+    desvirar2.classList.remove('rotacao');
+    contador = 0;
+    idCarta1 = 99;
+    idCarta2 = 99;
+    carta1 = 98;
+    carta2 = 99;
+}
+
+function boa() {
+    carta1 = 98;
+    carta2 = 99;
+    contador = 0;
+    paresAchados++
+    if (paresAchados == parseInt(quantidadeCartas) / 2) {
+        terminarContagem = 1;
+        alert(`Acabou você demorou apenas ${tempoDeJogo} segundos`)
+        let respostaReiniciar = prompt('Quer continuar? Sim/Não')
+        if (respostaReiniciar == "Sim") {
+            let cartaHtml = document.querySelector('section');
+            cartaHtml.innerHTML = ''
+            paresAchados = 0;
+            idCarta1 = 99;
+            idCarta2 = 99;
+            arrayCartas = [];
+            quantidadeCartas = 0;
+            terminarContagem = 0;
+            relogio.innerHTML = 0;
+            tempoDeJogo = 0;
+            funcaoQuantidadeCartas()
+        }
+        return
+    }
+}
+
+
+function embaralhar(array) {
+    array.sort(() => Math.random() - 0.5);
 }
